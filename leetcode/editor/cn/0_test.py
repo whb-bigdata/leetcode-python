80526a2c83d0382b33c0bcffb7d784632853d968
whb-bigdata <whb_bigdata@163.com>
2026-08-15 21:55:55 +1000

    add test

---
+++ leetcode/editor/cn/0_test.py
@@ -0,0 +1,28 @@
+from xxlimited_35 import Null
+
+
+class Node:
+    def __init__(self, data: int = 0, next=None):
+        self.data = data
+        self.next = next
+
+name_pool = ["A", "B", "C", "D"]
+
+singly_names = name_pool
+
+# 构建单链表
+singly_head = None
+singly_tail = None
+for name in singly_names:
+    new_node = Node(name)
+    if  singly_head is None:
+        singly_head = new_node
+        singly_tail = new_node
+    else:
+        singly_tail.next = new_node
+        singly_tail = new_node
+if __name__ == '__main__':
+    curr = singly_head
+    while curr.next is not None:
+        print(curr.data)
+        curr=curr.next
