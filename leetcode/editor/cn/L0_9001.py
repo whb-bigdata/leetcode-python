a8b03a1a4b4c6d0346e866ba6a0d9298f02223fb
whb-bigdata <whb_bigdata@163.com>
2026-09-14 13:37:23 +1000

    homework

--- leetcode/editor/cn/L0_9001.py
+++ leetcode/editor/cn/L0_9001.py
@@ -1,5 +1,31 @@
-s = 5 == 5.0
-print(s)
+letter_dict = {}
+f = True
+first = ''
+second = ''
+while f:
+    input_letter = input('Enter letter [A-Z]: ')
+    if input_letter != 'STOP':
+        if first == '':
+            first = input_letter
+        elif second == '':
+            second = input_letter
 
-s = '' + 8
-print(s)
+
+        if letter_dict.get(input_letter) is None:
+            print(f'New letter: {input_letter}!')
+            letter_dict[input_letter] = 1
+        elif letter_dict.get(input_letter) >= 2 and first == second and second == input_letter:
+            letter_dict.pop(input_letter)
+            print(f'Removing letter: {input_letter}!')
+        else:
+            letter_dict[input_letter] = letter_dict[input_letter] + 1
+
+        if first !='' and  second !='':
+            first = second
+            second = input_letter
+    else:
+        f = False
+print()
+print('Final Summary:')
+for key,value in letter_dict.items():
+    print(f'{key}: {value}')
